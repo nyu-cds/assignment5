4b4b73cc0248dd2739ddcc2b23fe1faa728a09a7
Greg Watson <g.watson@computer.org>
2017-03-08 07:32:15 -0500

    Update file1.py

--- file1.py
+++ file1.py
@@ -3,10 +3,9 @@ import itertools
 def kbits(n, k):
     result = []
     for bits in itertools.combinations(range(n), k):
-        s = ['0'] * n
+        s = ['1'] * n
         for bit in bits:
-            s[bit] = '1'
+            s[bit] = '0'
         result.append(''.join(s))
     return result
 
-print kbits(4, 3)
